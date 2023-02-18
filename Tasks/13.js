6e4164bfd50610a81b8451bd482648eab593391a
irfanali <irfu3844@gmail.com>
2023-02-18 12:18:30 +0530

    18-02-2023 BROTOTYPE

--- Tasks/13.js
+++ Tasks/13.js
@@ -6,3 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const prompt_sync_1 = __importDefault(require("prompt-sync"));
 let prompt = (0, prompt_sync_1.default)();
 let string = prompt("ENTER THE STRING : ");
+let string2 = "";
+string2 = string.split("").reverse().join("");
+if (string === string2) {
+    console.log("Entered string is palindrome");
+}
+else {
+    console.log("Entered string is not palindrome");
+}
--- Tasks/13.ts
+++ Tasks/13.ts
@@ -3,3 +3,13 @@ let prompt=PromptSync()
 
 let string:string=prompt("ENTER THE STRING : ")
 
+let string2= "";
+string2 = string.split("").reverse().join("");
+
+
+if(string===string2){
+console.log("Entered string is palindrome");
+}
+else{
+console.log("Entered string is not palindrome");
+}
